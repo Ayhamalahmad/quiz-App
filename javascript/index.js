53dfb7dddaa045a0d1d7b2217a8c2c606c55251b
Ayhamalahmad <amadae130@gmail.com>
2023-09-06 00:09:12 +0300

    .

--- javascript/index.js
+++ javascript/index.js
@@ -1,17 +1,67 @@
 // Select relevant elements
 let welcomeMassege = document.querySelector(".welcome-massege");
 // Set interval
-//
-let options = document.querySelectorAll(".op");
-let currentIndex = 0;
-// Function to update the active option
-const updateActiveOption = () => {
-  // Remove the "active" class from the current active option
-  options[currentIndex].classList.remove("active");
-  // Increment the index or reset it if it exceeds the number of options
-  currentIndex = (currentIndex + 1) % options.length;
-  // Add the "active" class to the new active option
-  options[currentIndex].classList.add("active");
+// Animate Text
+let i = 0;
+let milliseconds = 100;
+let editorTitle = "Welcome to the Quiz App!";
+function startTextAnimation() {
+  let textInterval = setInterval(() => {
+    welcomeMassege.textContent += editorTitle[i];
+    i++;
+    if (i === editorTitle.length) {
+      i = 0;
+      clearInterval(textInterval);
+      setTimeout(() => {
+        welcomeMassege.textContent = "";
+        startTextAnimation();
+      }, 2000);
+    }
+  }, milliseconds);
+}
+startTextAnimation();
+
+// // options
+// let options = document.querySelectorAll(".op");
+// let difficultys = document.querySelectorAll(".di");
+// let currentIndex = 0;
+// // Function to update the active option
+// const updateActiveOption = (section) => {
+//   // Remove the "active" class from the current active option
+//   section[currentIndex].classList.remove("active");
+//   // Increment the index or reset it if it exceeds the number of options
+//   currentIndex = (currentIndex + 1) % section.length;
+//   // Add the "active" class to the new active option
+//   section[currentIndex].classList.add("active");
+// };
+// // Set an interval to change the active option every 2 seconds (2000 milliseconds)
+// setInterval(() => {
+//   updateActiveOption(options);
+// }, 2000);
+// setInterval(() => {
+//   updateActiveOption(difficultys);
+// }, 2000);
+
+// Function to update the active option for both category and difficulty
+const updateActiveOptions = () => {
+  // Find the currently active category and difficulty options
+  const activeCategory = document.querySelector(".op.active");
+  const activeDifficulty = document.querySelector(".di.active");
+
+  // Remove the "active" class from the current active category and difficulty options
+  activeCategory.classList.remove("active");
+  activeDifficulty.classList.remove("active");
+
+  // Find the next category and difficulty options
+  const nextCategory =
+    activeCategory.nextElementSibling || document.querySelector(".op");
+  const nextDifficulty =
+    activeDifficulty.nextElementSibling || document.querySelector(".di");
+
+  // Add the "active" class to the new active category and difficulty options
+  nextCategory.classList.add("active");
+  nextDifficulty.classList.add("active");
 };
-// Set an interval to change the active option every 2 seconds (2000 milliseconds)
-setInterval(updateActiveOption, 2000);
+
+// Set an interval to update both category and difficulty options every 2 seconds (2000 milliseconds)
+setInterval(updateActiveOptions, 2000);
