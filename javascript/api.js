aab2799cc746c0b1a9daf75f2055574c4665c57d
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-09-09 19:05:21 +0300

    Construct the apiUrl with updated parameters

--- javascript/api.js
+++ javascript/api.js
@@ -1,22 +1,20 @@
 // https://opentdb.com/api_config.php
 let categoryOptionsContainer = document.querySelector(".category-options");
+let dropdownS = document.querySelectorAll(".dropdown div");
 let numberOfQ = 10;
 let selectedCategoryId;
-let apiUrl = `https://opentdb.com/api.php?amount=${numberOfQ}&category=${selectedCategoryId}`;
-const apiUrlAll = `https://opentdb.com/api.php?amount=${numberOfQ}`;
+let difficultyLevel;
+let apiUrl = `https://opentdb.com/api.php?amount=${numberOfQ}`;
 const apiUrlCategories = "https://opentdb.com/api_category.php";
-let categoriesNames = [];
-let apiUrlDataNmae = [];
-let apiUrlAllDataNmae = [];
 let promises = [
   fetch(apiUrlCategories).then((response) => response.json()),
   fetch(apiUrl).then((response) => response.json()),
-  // fetch(apiUrlAll).then((response) => response.json()),
 ];
 Promise.all(promises)
   .then((results) => {
     let categoriesData = results[0];
     let apiUrlData = results[1];
+
     // let apiUrlAllData = results[2];
     // Loop through each element in the trivia_categories
     categoriesData.trivia_categories.forEach((element) => {
@@ -39,17 +37,29 @@ Promise.all(promises)
     // Handle any errors that occurred during the fetch
     console.log("Fetch error", error);
   });
-// Add a click event listener to the  category Options container
-categoryOptionsContainer.addEventListener("click", (e) => {
-  // Check if the clicked element has the class "category-option"
-  if (e.target.classList.contains("category-option")) {
-    // Log the ID of the clicked element
-    console.log(e.target.id);
-    selectedCategoryId = e.target.id;
-    apiUrl = `https://opentdb.com/api.php?amount=${numberOfQ}&category=${selectedCategoryId}`;
-    if(e.target.classList.contains("category-option-all")){
-      apiUrl = `https://opentdb.com/api.php?amount=${numberOfQ}`;
+
+// Add a click event listener to the container
+dropdownS.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    // Check if the clicked element has the class "category-option"
+    if (e.target.classList.contains("category-option")) {
+      // Log the ID of the clicked element
+      console.log(e.target.id);
+      selectedCategoryId = e.target.id;
     }
+    // If a questions option is clicked, update the number of questions based on the data attribute
+    if (e.target.classList.contains("questions-option")) {
+      numberOfQ = parseInt(e.target.dataset.numberq);
+    }
+    // If a difficulties option is clicked, update the difficulty level based on the data attribute
+    if (e.target.classList.contains("difficulties-option")) {
+      difficultyLevel = e.target.dataset.difficulty;
+    }
+    // Construct the apiUrl with updated parameters based on user selections
+    apiUrl = `https://opentdb.com/api.php?amount=${numberOfQ}${
+      selectedCategoryId ? `&category=${selectedCategoryId}` : ""
+    }${difficultyLevel ? `&difficulty=${difficultyLevel}` : ""}`;
+
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
@@ -58,5 +68,5 @@ categoryOptionsContainer.addEventListener("click", (e) => {
       .catch((error) => {
         console.error("Fetch error", error);
       });
-  }
+  });
 });
