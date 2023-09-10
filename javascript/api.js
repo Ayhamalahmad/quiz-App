1222c3240bcff5609e5422a4fab4b0b173a256cf
Ayham Alahmad <86928915+Ayhamalahmad@users.noreply.github.com>
2023-09-10 11:10:20 +0300

    .

--- javascript/api.js
+++ javascript/api.js
@@ -1,14 +1,25 @@
 // https://opentdb.com/api_config.php
 let categoryOptionsContainer = document.querySelector(".category-options");
 let dropdownS = document.querySelectorAll(".dropdown div");
+// Questions
+let questionNumber = document.querySelector(".question-container .num");
+let buttonsContainer = document.querySelectorAll(".buttons button");
+let getquestionAnswerContainer = document.querySelectorAll(
+  ".question-answer-container"
+);
+console.log(getquestionAnswerContainer);
+
+// Vars
+let currentIndex = 0;
 let numberOfQ = 10;
 let selectedCategoryId;
 let difficultyLevel;
+let questionAnswerContainer;
 let apiUrl = `https://opentdb.com/api.php?amount=${numberOfQ}`;
 const apiUrlCategories = "https://opentdb.com/api_category.php";
 let promises = [
   fetch(apiUrlCategories).then((response) => response.json()),
-  fetch(apiUrl).then((response) => response.json()),
+  // fetch(apiUrl).then((response) => response.json()),
 ];
 Promise.all(promises)
   .then((results) => {
@@ -43,8 +54,6 @@ dropdownS.forEach((dropdown) => {
   dropdown.addEventListener("click", (e) => {
     // Check if the clicked element has the class "category-option"
     if (e.target.classList.contains("category-option")) {
-      // Log the ID of the clicked element
-      console.log(e.target.id);
       selectedCategoryId = e.target.id;
     }
     // If a questions option is clicked, update the number of questions based on the data attribute
@@ -63,10 +72,74 @@ dropdownS.forEach((dropdown) => {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
+
         console.log("Data:", data);
+        
+        data.results.forEach((result) => {
+
+          // console.log(result.question);
+          questionNumber.textContent = numberOfQ;
+          // Create a paragraph for the question text
+          const questionText = document.createElement("p");
+          questionText.classList.add("question-text");
+          questionText.textContent = result.question;
+
+          // Create a header for answer choices
+          const answerHeader = document.createElement("h3");
+          answerHeader.classList.add("answer-header");
+          answerHeader.textContent = "Choose an answer:";
+
+          // Create an unordered list for answer choices
+          const answerList = document.createElement("ul");
+          answerList.classList.add("answer-choices");
+
+          // Create list items for answer choices and add them to the list
+          const choices = [result.correct_answer, ...result.incorrect_answers];
+          choices.forEach((choiceText) => {
+            const listItem = document.createElement("li");
+            listItem.classList.add("answer-choice");
+            listItem.textContent = choiceText;
+            answerList.appendChild(listItem);
+          });
+
+          // Add the created elements to the DOM
+          const questionContainer = document.querySelector(
+            ".question-container"
+          );
+          questionAnswerContainer = document.createElement("div");
+          questionAnswerContainer.classList.add("question-answer-container");
+          questionAnswerContainer.appendChild(questionText);
+          questionAnswerContainer.appendChild(answerHeader);
+          questionAnswerContainer.appendChild(answerList);
+          questionContainer.appendChild(questionAnswerContainer);
+       
+        });
       })
       .catch((error) => {
         console.error("Fetch error", error);
       });
   });
 });
+
+// //
+// buttonsContainer.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     console.log(e.target);
+//     if (e.target.classList.contains("next")) {
+//       currentIndex++;
+//     } else if (e.target.classList.contains("previous")) {
+//       currentIndex--;
+//     }
+//     console.log(currentIndex);
+//   });
+// });
+
+// getquestionAnswerContainer.forEach((e, index) => {
+//   e.classList.remove("active");
+//   if (index === currentIndex) {
+//     e.classList.add("active");
+//   } else {
+//     e.classList.remove("active");
+//   }
+// });
+console.log(getquestionAnswerContainer);
